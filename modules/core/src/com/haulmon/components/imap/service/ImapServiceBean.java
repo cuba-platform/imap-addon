935b8454f66f3a772ae2af3c3e2bfa0043c17cdf
maxim.shmelev <shmelyov.maksim@gmail.com>
2018-01-09 12:09:24 +0400

    IMAP-21 Configuration. Screen for editing mailbox. Root Certificate

--- modules/core/src/com/haulmon/components/imap/service/ImapServiceBean.java
+++ modules/core/src/com/haulmon/components/imap/service/ImapServiceBean.java
@@ -2,14 +2,29 @@ package com.haulmon.components.imap.service;
 
 import com.haulmon.components.imap.entity.MailBox;
 import com.haulmon.components.imap.entity.MailSecureMode;
+import com.haulmont.cuba.core.global.FileLoader;
+import com.haulmont.cuba.core.global.FileStorageException;
 import com.sun.mail.util.MailSSLSocketFactory;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
 import javax.mail.Folder;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
@@ -17,6 +32,9 @@ import java.util.stream.Collectors;
 
 @Service(ImapService.NAME)
 public class ImapServiceBean implements ImapService {
+
+    @Inject
+    private FileLoader fileLoader;
 
     @Override
     public void testConnection(MailBox box) throws MessagingException {
@@ -51,13 +69,77 @@ public class ImapServiceBean implements ImapService {
     }
 
     private MailSSLSocketFactory getMailSSLSocketFactory(MailBox box) throws MessagingException {
-        MailSSLSocketFactory socketFactory= null;
+        MailSSLSocketFactory socketFactory = null;
         try {
             socketFactory = new MailSSLSocketFactory();
         } catch (GeneralSecurityException e) {
             throw new MessagingException("SSL Socket factory exception", e);
         }
-        socketFactory.setTrustAllHosts(true);
+        if (box.getRootCertificate() != null) {
+            try ( InputStream rootCert = fileLoader.openStream(box.getRootCertificate()) ) {
+                socketFactory.setTrustManagers(new TrustManager[] {new UnifiedTrustManager(rootCert) });
+            } catch (FileStorageException | GeneralSecurityException | IOException e) {
+                e.printStackTrace(); //todo:
+            }
+        }
         return socketFactory;
+    }
+
+    private static class UnifiedTrustManager implements X509TrustManager {
+        private X509TrustManager defaultTrustManager;
+        private X509TrustManager localTrustManager;
+
+        public UnifiedTrustManager(InputStream rootCertStream) {
+            try {
+                this.defaultTrustManager = createTrustManager(null);
+
+                CertificateFactory cf = CertificateFactory.getInstance("X.509");
+                Certificate ca = cf.generateCertificate(rootCertStream);
+
+                // Create a KeyStore containing our trusted CAs
+                String keyStoreType = KeyStore.getDefaultType();
+                KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+                keyStore.load(null, null);
+                keyStore.setCertificateEntry("rootCA", ca);
+
+                this.localTrustManager = createTrustManager(keyStore);
+            } catch (NoSuchAlgorithmException | CertificateException | KeyStoreException | IOException e) {
+                e.printStackTrace(); //todo:
+            }
+        }
+
+        private X509TrustManager createTrustManager(KeyStore store) throws NoSuchAlgorithmException, KeyStoreException {
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(store);
+            TrustManager[] trustManagers = tmf.getTrustManagers();
+            return (X509TrustManager) trustManagers[0];
+        }
+
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            try {
+                defaultTrustManager.checkServerTrusted(chain, authType);
+            } catch (CertificateException ce) {
+                localTrustManager.checkServerTrusted(chain, authType);
+            }
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            try {
+                defaultTrustManager.checkClientTrusted(chain, authType);
+            } catch (CertificateException ce) {
+                localTrustManager.checkClientTrusted(chain, authType);
+            }
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            X509Certificate[] first = defaultTrustManager.getAcceptedIssuers();
+            X509Certificate[] second = localTrustManager.getAcceptedIssuers();
+            X509Certificate[] result = Arrays.copyOf(first, first.length + second.length);
+            System.arraycopy(second, 0, result, first.length, second.length);
+            return result;
+        }
     }
 }
