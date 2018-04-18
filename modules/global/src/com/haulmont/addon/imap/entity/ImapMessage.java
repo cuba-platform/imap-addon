0b0344df1e0cb68105f769768ddb255ca8f035ff
shmelev-haulmont <shmelev@haulmont.com>
2018-04-18 09:57:32 +0400

    #11 @NamePattern for ImapMessage

--- modules/global/src/com/haulmont/addon/imap/entity/ImapMessage.java
+++ modules/global/src/com/haulmont/addon/imap/entity/ImapMessage.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@NamePattern("/%s from |msgUid")
+@NamePattern("%s (#%d) | caption, msgNum")
 @Table(name = "IMAP_MESSAGE")
 @Entity(name = "imap$Message")
 public class ImapMessage extends StandardEntity {
