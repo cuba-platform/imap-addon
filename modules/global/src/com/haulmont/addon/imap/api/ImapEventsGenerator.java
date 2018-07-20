ddb46aad92dc781ffde7248ce0cd4e56e248ea94
smirnova <smirnova@haulmont.com>
2018-07-20 17:30:29 +0400

    Updated javadocs

--- modules/global/src/com/haulmont/addon/imap/api/ImapEventsGenerator.java
+++ modules/global/src/com/haulmont/addon/imap/api/ImapEventsGenerator.java
@@ -8,26 +8,26 @@ import com.haulmont.addon.imap.events.NewEmailImapEvent;
 import java.util.Collection;
 
 /**
- * Extension point for IMAP events. Bean implementing this interface can be specified in IMAP mailbox configuration.
- * Such beans can be useful for using IMAP extensions and custom communication mechanisms specific
+ * An extension point for IMAP events. A bean implementing this interface can be specified in an IMAP configuration.
+ * Such beans can be useful for applying IMAP extensions and custom communication mechanisms specific
  * for particular mailboxes
  */
 public interface ImapEventsGenerator {
 
     /**
-     * Perform bootstrap logic for mailbox synchronization, e.g. attach listeners or schedule background tasks
+     * Performs bootstrap logic for mailbox synchronization, e.g. attaching listeners or schedule background tasks
      *
      * @param mailBox IMAP mailbox
      */
     void init(ImapMailBox mailBox);
     /**
-     * Release resources used for synchronization, e.g. detach listeners or cancel scheduled background tasks
+     * Releases resources used for synchronization, e.g. detaching listeners or cancelling scheduled background tasks
      *
      * @param mailBox IMAP mailbox
      */
     void shutdown(ImapMailBox mailBox);
     /**
-     * Emit events for new messages in mailbox folder accumulated since the previous call of this method for the folder
+     * Emits events for new messages in a mailbox folder accumulated since the previous call of this method for the folder
      *
      * @param folder IMAP mailbox folder
      * @return       events related to new messages in the folder,
@@ -36,7 +36,7 @@ public interface ImapEventsGenerator {
      */
     Collection<? extends BaseImapEvent> generateForNewMessages(ImapFolder folder);
     /**
-     * Emit events for modified messages in mailbox folder accumulated since the previous call of this method
+     * Emits events for modified messages in a mailbox folder accumulated since the previous call of this method
      * for the folder
      *
      * @param folder IMAP mailbox folder
@@ -45,7 +45,7 @@ public interface ImapEventsGenerator {
      */
     Collection<? extends BaseImapEvent> generateForChangedMessages(ImapFolder folder);
     /**
-     * Emit events for missed (moved to other folder or deleted) messages in mailbox folder accumulated since the
+     * Emits events for missed (moved to other folder or deleted) messages in a mailbox folder accumulated since the
      * previous call of this method for the folder
      *
      * @param folder IMAP mailbox folder
