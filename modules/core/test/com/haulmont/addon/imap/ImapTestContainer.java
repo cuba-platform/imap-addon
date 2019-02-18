083417e7bbb1901a3328d969ab7a7e0d0b7da277
zaharchenko <zaharchenko@haulmont.com>
2019-02-18 13:05:05 +0400

    Upgrade component to CUBA platform 7.0.0 #75
    fix tests

--- modules/core/test/com/haulmont/addon/imap/ImapTestContainer.java
+++ modules/core/test/com/haulmont/addon/imap/ImapTestContainer.java
@@ -32,7 +32,6 @@ public class ImapTestContainer extends TestContainer {
                 // or add another one in the end.
                 "com/haulmont/addon/imap/test-app.properties");
         initDbProperties();
-        dbUrl = "jdbc:hsqldb:hsql://localhost/imap";
     }
 
     private void initDbProperties() {
