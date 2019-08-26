45b428e2e8c1b7e5f94acb296f5266242aa52a01
zaharchenko <zaharchenko@haulmont.com>
2019-08-26 15:06:39 +0400

    Add Oracle, MS SQL, MySQL init scripts #86
    fix MS SQL init script
    
    (cherry picked from commit 6a37bf79529fff55cd41aac1e25d8eef906fed91)

--- modules/core/db/init/mssql/30.create-db.sql
+++ modules/core/db/init/mssql/30.create-db.sql
@@ -1,6 +1,6 @@
 insert into SYS_SCHEDULED_TASK
 (ID, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, DEFINED_BY, BEAN_NAME, METHOD_NAME, CLASS_NAME, SCRIPT_NAME, USER_NAME, IS_SINGLETON, IS_ACTIVE, PERIOD, TIMEOUT, START_DATE, CRON, SCHEDULING_TYPE, TIME_FRAME, START_DELAY, PERMITTED_SERVERS, LOG_START, LOG_FINISH, LAST_START_TIME, LAST_START_SERVER, METHOD_PARAMS, DESCRIPTION)
-values ('1082fe3b-3b75-d2f2-d858-dbae487c4549', '2019-03-22 10:26:18', 'admin', '2019-03-22 10:26:18', null, null, null, 'B', 'imap_ImapScheduler', 'syncImap', null, null, null, null, null, 30, null, null, null, 'P', null, null, null, true, true, null, null, '<?xml version="1.0" encoding="UTF-8"?>
+values ('1082fe3b-3b75-d2f2-d858-dbae487c4549', '2019-03-22 10:26:18', 'admin', '2019-03-22 10:26:18', null, null, null, 'B', 'imap_ImapScheduler', 'syncImap', null, null, null, null, null, 30, null, null, null, 'P', null, null, null, 1, 1, null, null, '<?xml version="1.0" encoding="UTF-8"?>
 
 <params/>
 ', null);
